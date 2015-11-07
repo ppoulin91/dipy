cc89b4f5693bba81464bf0928ed93094eb8f0df6
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2015-11-07 14:50:52 -0500

    Added new simulated streamlines in test_qbx.py

--- dipy/segment/tests/test_qbx.py
+++ dipy/segment/tests/test_qbx.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 
 from dipy.segment.clusteringspeed import QuickBundlesX
@@ -7,27 +8,70 @@ from dipy.data import get_data
 import nibabel.trackvis as tv
 
 
-def test_basic_qbx():
+def generate_straight_bundle(nb_streamlines=1, nb_pts=30, step_size=1, radius=1, rng=np.random.RandomState(42)):
+    bundle = []
 
-    feature_shape = (10, 3)
-    streamlines = [np.ones((10, 3), dtype="f4")]
-    #test_hqb(streamlines, AveragePointwiseEuclideanMetric())
+    bundle_length = step_size * nb_pts
 
-    feature_shape = (1, 3)
-    points = np.array([[[1, 1, 0]],
-                       [[3, 1, 0]],
-                       [[2, 1, 0]],
-                       [[5, 1, 0]],
-                       [[5.5, 1, 0]]], dtype="f4")
+    Z = -np.linspace(0, bundle_length, nb_pts)
+    for k in range(nb_streamlines):
+        theta = rng.rand() * (2*np.pi)
+        r = radius * rng.rand()
 
-    thresholds = [4, 2, 1]
-    qbx = QuickBundlesX(feature_shape, thresholds, AveragePointwiseEuclideanMetric())
-    print(qbx)
+        Xk = np.ones(nb_pts) * (r * np.cos(theta))
+        Yk = np.ones(nb_pts) * (r * np.sin(theta))
+        Zk = Z.copy()
 
-    for i, p in enumerate(points):
-        print "\nInserting {}".format(p)
-        qbx.insert(p, np.int32(i))
-        print(qbx)
+        bundle.append(np.c_[Xk, Yk, Zk])
+
+    return bundle
+
+
+def generate_bearing_bundles(nb_balls=6, bearing_radius=2):
+    bundles = []
+
+    for theta in np.linspace(0, 2*np.pi, nb_balls, endpoint=False):
+        x = bearing_radius * np.cos(theta)
+        y = bearing_radius * np.sin(theta)
+
+        bundle = np.array(generate_straight_bundle(nb_streamlines=100))
+        bundle += (x, y, 0)
+        bundles.append(bundle)
+
+    return bundles
+
+
+def gen_streamlines_in_circle(nb_streamlines=1, nb_pts=30, step_size=1, radius=1):
+    bundle = []
+
+    bundle_length = step_size * nb_pts
+
+    Z = np.linspace(0, bundle_length, nb_pts)
+    theta = 0
+    for theta in np.linspace(0, 2*np.pi, nb_streamlines, endpoint=False):
+        Xk = np.ones(nb_pts) * (radius * np.cos(theta))
+        Yk = np.ones(nb_pts) * (radius * np.sin(theta))
+        Zk = Z.copy()
+
+        bundle.append(np.c_[Xk, Yk, Zk])
+
+    return bundle
+
+
+def gen_streamlines_parallel(nb_streamlines=1, nb_pts=30, step_size=1, delta=1):
+    bundle = []
+
+    bundle_length = step_size * nb_pts
+
+    Z = np.linspace(0, bundle_length, nb_pts)
+    for x in delta*np.arange(0, nb_streamlines):
+        Xk = np.ones(nb_pts) * x
+        Yk = np.zeros(nb_pts)
+        Zk = Z.copy()
+
+        bundle.append(np.c_[Xk, Yk, Zk])
+
+    return bundle
 
 
 def simulated_bundle(no_streamlines=10, waves=False, no_pts=12):
@@ -52,6 +96,47 @@ def fornix_streamlines(no_pts=12):
     return streamlines
 
 
+def test_3D_segments():
+    feature_shape = (2, 3)
+    points = np.array([[[1, 0, 0],
+                        [1, 1, 0]],
+                       [[3, 0, 0],
+                        [3, 1, 0]],
+                       [[2, 0, 0],
+                        [2, 1, 0]],
+                       [[5, 0, 0],
+                        [5, 1, 0]],
+                       [[5.5, 0, 0],
+                        [5.5, 1, 0]]], dtype="f4")
+
+    thresholds = [4, 2, 1]
+    qbx = QuickBundlesX(feature_shape, thresholds, AveragePointwiseEuclideanMetric())
+    print(qbx)
+
+    for i, p in enumerate(points):
+        print "\nInserting {}".format(p)
+        qbx.insert(p, np.int32(i))
+        print(qbx)
+
+
+def test_3D_points():
+    feature_shape = (1, 3)
+    points = np.array([[[1, 0, 0]],
+                       [[3, 0, 0]],
+                       [[2, 0, 0]],
+                       [[5, 0, 0]],
+                       [[5.5, 0, 0]]], dtype="f4")
+
+    thresholds = [4, 2, 1]
+    qbx = QuickBundlesX(feature_shape, thresholds, AveragePointwiseEuclideanMetric())
+    print(qbx)
+
+    for i, p in enumerate(points):
+        print "\nInserting {}".format(p)
+        qbx.insert(p, np.int32(i))
+        print(qbx)
+
+
 def test_with_simulated_bundles():
 
     streamlines = simulated_bundle(100, False, 20)
@@ -65,7 +150,20 @@ def test_with_simulated_bundles():
     window.show(renderer)
 
 
+def test_with_simulated_bundles2():
+    # Generate synthetic streamlines
+    bundles = generate_bearing_bundles()
+    bundles.append(generate_straight_bundle(100))
+    streamlines = list(itertools.chain(*bundles))
+
+    from dipy.viz import actor, window
+
+    renderer = window.Renderer()
+    bundle_actor = actor.line(streamlines)
+    renderer.add(bundle_actor)
+
+    window.show(renderer)
+
+
 if __name__ == '__main__':
-
-    test_with_simulated_bundles()
-
+    test_with_simulated_bundles2()
