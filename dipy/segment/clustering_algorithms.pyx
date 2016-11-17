09022301dc5483f6d0cf54241e99e88559b51713
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2016-11-16 21:06:35 -0500

    Added new script that finds closest streamlines to those contained in a reference tractogram

--- dipy/segment/clustering_algorithms.pyx
+++ dipy/segment/clustering_algorithms.pyx
@@ -6,7 +6,7 @@ import numpy as np
 
 from cythonutils cimport Data2D, shape2tuple
 from metricspeed cimport Metric
-from clusteringspeed cimport ClustersCentroid, Centroid, QuickBundles
+from clusteringspeed cimport Clusters, ClustersCentroid, Centroid, QuickBundles
 from dipy.segment.clustering import ClusterMapCentroid, ClusterCentroid
 
 cdef extern from "stdlib.h" nogil:
@@ -43,7 +43,10 @@ def clusters_centroid2clustermap_centroid(ClustersCentroid clusters_list):
     clusters = ClusterMapCentroid()
     for i in range(clusters_list._nb_clusters):
         centroid = np.asarray(clusters_list.centroids[i].features)
-        indices = np.asarray(<int[:clusters_list.clusters_size[i]]> clusters_list.clusters_indices[i]).tolist()
+        indices = np.array([], dtype=np.int32)
+        if clusters_list.clusters_size[i] > 0:
+            indices = np.asarray(<int[:clusters_list.clusters_size[i]]> clusters_list.clusters_indices[i]).tolist()
+
         clusters.add_cluster(ClusterCentroid(id=i, centroid=centroid, indices=indices))
 
     return clusters
@@ -113,3 +116,70 @@ def quickbundles(streamlines, Metric metric, double threshold, long max_nb_clust
         qb.update_step(cluster_id)
 
     return clusters_centroid2clustermap_centroid(qb.clusters)
+
+
+def quickbundles_assignment(clusters, streamlines, Metric metric, double threshold, ordering=None):
+    """ Assigns streamlines to nearest clusters.
+
+    Note that the clusters won't be updated.
+
+    Parameters
+    ----------
+    clusters : `ClusterMapCentroid` object
+        Clusters for which the streamlines will be assigned to.
+    streamlines : list of 2D arrays
+        List of streamlines to cluster.
+    metric : `Metric` object
+        Tells how to compute the distance between two streamlines.
+    threshold : double
+        The maximum distance from a cluster for a streamline to be still
+        considered as part of it.
+    ordering : iterable of indices, optional
+        Iterate through `data` using the given ordering.
+
+    Returns
+    -------
+    `ClusterMapCentroid` object
+        Update version of the `clusters`.
+    """
+    # Threshold of np.inf is not supported, set it to 'biggest_double'
+    threshold = min(threshold, BIGGEST_DOUBLE)
+    # Threshold of -np.inf is not supported, set it to 0
+    threshold = max(threshold, 0)
+
+    if ordering is None:
+        ordering = xrange(len(streamlines))
+
+    # Check if `ordering` or `streamlines` are empty.
+    first_idx, ordering = peek(ordering)
+    if first_idx is None or len(streamlines) == 0:
+        return clusters
+
+    max_nb_clusters = len(clusters)  # We don't want to create new clusters.
+    features_shape = shape2tuple(metric.feature.c_infer_shape(streamlines[first_idx].astype(DTYPE)))
+    cdef QuickBundles qb = QuickBundles(features_shape, metric, threshold, max_nb_clusters)
+    cdef int idx, i, n, d
+
+    # Initialize centroids using the ones from `clusters`.
+    for i, cluster in enumerate(clusters):
+        qb.clusters.c_create_cluster()
+        for n in range(features_shape[0]):
+            for d in range(features_shape[1]):
+                qb.clusters.centroids[i].features[n][d] = cluster.centroid[n, d]
+
+    # Create special cluster where to put unassigned streamlines
+    unassigned_cluster_id = qb.clusters.c_create_cluster()
+    print("asdsadsadasdsadadasdsad", unassigned_cluster_id)
+
+    for idx in ordering:
+        streamline = streamlines[idx]
+        if not streamline.flags.writeable or streamline.dtype != DTYPE:
+            streamline = streamline.astype(DTYPE)
+
+        # We do assignation only, no centroid will be updated.
+        cluster_id = qb.assignment_step(streamline, idx, unassigned_cluster_id)
+
+    print("Nb. clusters:", qb.clusters.c_size())
+    new_clusters = clusters_centroid2clustermap_centroid(qb.clusters)
+    new_clusters.refdata = streamlines
+    return new_clusters
